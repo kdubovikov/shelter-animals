4dd1424e23452d8ae244f8937335e712d07439f5
Kirill Dubovikov <dubovikov.kirill@gmail.com>
2016-05-13 01:16:49 +0300

    stacking, intermediate commit

--- python_scripts/create_model.py
+++ python_scripts/create_model.py
@@ -1,31 +1,32 @@
-from clean import clean
-import pandas as pd
-import xgboost as xgb
-from sklearn import preprocessing
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.preprocessing import LabelEncoder
-import numpy as np
 import logging
 
+import numpy as np
+import pandas as pd
+import xgboost as xgb
+from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
+from xgboost import XGBClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn.cross_validation import StratifiedKFold
+
+from clean import clean
+
 if __name__ == '__main__':
+    # Data preparation
     logging.basicConfig(level=logging.INFO)
     logging.info('Loading datasets...')
-    train = pd.read_csv("../train.csv")
-    test = pd.read_csv("../test.csv")
+    train_idx = pd.read_csv("../train.csv")
+    test_idx = pd.read_csv("../test.csv")
 
     logging.info('Cleaning train dataset...')
-    train_x = clean(train)
-    train_y = train.loc[:, "OutcomeType"]
+    train_x = clean(train_idx)
+    train_y = train_idx.loc[:, "OutcomeType"]
 
     enc = LabelEncoder()
     enc.fit(train_y)
     train_yt = enc.transform(train_y)
 
     logging.info('Cleaning test dataset...')
-    test_x = clean(test)
-
-    # for diff in train_x.columns.difference(test_x.columns):
-    #     test_x[diff] = 0
+    test_x = clean(test_idx)
 
     for diff in test_x.columns.difference(train_x.columns):
         test_x[diff] = 0
@@ -36,9 +37,8 @@ if __name__ == '__main__':
     train_x.sort_index(axis=1, inplace=True)
     test_x.sort_index(axis=1, inplace=True)
 
-    # logging.info(train_x.head(10))
-
-    print(test_x.columns.difference(train_x.columns))
+    # Model creation
+    skf = list(StratifiedKFold(train_yt, 10))
 
     xgb_params = {
                     'learning_rate': 0.2,
@@ -50,37 +50,27 @@ if __name__ == '__main__':
                     'num_boost_rounds': 200,
                     'eval_metric': 'logloss'}
 
-    # bst = xgb.train(xgb_params, xgb.DMatrix(train_x, train_yt))
-    # fscores = bst.get_fscore()
-    #
-    # filtered_fscores_t = {k: v for k, v in fscores.items() if v > 0}
-    # filtered_cols_t = list(filtered_fscores_t.keys())
-    #
-    # train_xf = train_x[filtered_cols_t]
-    # # val_xr = val_x[filtered_cols]
-    # test_xf = test_x[filtered_cols_t]
+    # TODO: logloss eval metric for XGBoost
+    clfs = [RandomForestClassifier(n_estimators=300, criterion='gini', n_jobs=8),
+           ExtraTreesClassifier(n_estimators=500, n_jobs=-1, criterion='gini'),
+           XGBClassifier(n_estimators=500, learning_rate=0.2, max_depth=6, objective='multi:softprob', subsample=0.8)]
 
-    boosters = np.array([])
-    predictions = []
+    train_blend = np.zeroes(train_x.shape[0], len(clfs))
+    test_blend = np.zeroes(test_x.shape[0], len(clfs))
 
-    # print(xgb.cv(xgb_params, xgb.DMatrix(train_x, train_yt), nfold=5, num_boost_round=10, early_stopping_rounds=10, metrics=["mlogloss"], verbose_eval=False))
+    for (j, clf) in enumerate(clfs):
+        test_blend_j = np.zeros(test_x.shape[0], len(skf))
 
-    for i in range(0, 15):
-        print("Fitting XGB model #%d" % i)
-        booster = xgb.train(xgb_params, num_boost_round=200, dtrain=xgb.DMatrix(train_x, train_yt), verbose_eval=False)
-        boosters = np.append(boosters, booster)
-        predictions.append(booster.predict(xgb.DMatrix(test_x)))
+        for i, (train_idx, test_idx) in skf:
+            x = train_x[train_idx]
+            y = train_y[train_idx]
+            xt = test_x[test_idx]
 
-    rf_models = np.array([])
+            clf.fit(x, y)
+            preds = clf.predict_proba(xt)[:, 1]
+            train_blend[]
 
-    for i in range(0, 10):
-        print("Fitting RF model #%d" % i)
-        rf_clf = RandomForestClassifier(n_estimators=300, criterion='gini', n_jobs=8)
-        rf_clf.fit(train_x, train_yt)
-        rf_models = np.append(rf_models, rf_clf)
-        predictions.append(rf_clf.predict_proba(test_x))
-
-    preds = np.mean(predictions, axis=0)
+    # Saving results
     final_results = pd.DataFrame(preds)
     final_results.columns = ['Adoption', 'Died', 'Euthanasia', 'Return_to_owner', 'Transfer']
     final_results.index.name = 'ID'
@@ -88,4 +78,4 @@ if __name__ == '__main__':
 
     final_results = final_results[['Adoption', 'Died', 'Euthanasia', 'Return_to_owner', 'Transfer']]
 
-    final_results.to_csv('results_stacking.csv')
+    final_results.to_csv('results_averaging.csv')
